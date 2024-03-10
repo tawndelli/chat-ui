c067b077d7b9b8b0619f505a19c56f1cdf3d8e96
tawndelli <gf1006@hotmail.com>
2024-03-10 18:52:34 -0500

    adding cloudrun endpoint url

--- index.js
+++ index.js
@@ -27,13 +27,14 @@ inputForm.addEventListener('submit', function(event) {
   message.scrollIntoView({behavior: "smooth"});
 
   // Get chatbot response
-  fetch('http://localhost:8000/prompt/', {
+  fetch('https://rights-sichvtpofq-uc.a.run.app/prompt/', {
     method: "POST",
     body: JSON.stringify({
         query_str: input
     }),
     headers: {
-        "Content-type": "application/json; charset=UTF-8"
+        "Content-type": "application/json; charset=UTF-8",
+        "Access-Control-Allow-Origin": "*"
       }
   }).then((response) => response.json())
   .then((json) => {
